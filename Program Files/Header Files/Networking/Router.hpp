c4a13bc9cd94ad988f2053b5ba7f64a360e93a7d
Muhammad2775 <muhammad.moazzam2775@gmail.com>
2026-05-16 13:24:12 -0700

    Refactor Router class with additional components

--- Program Files/Header Files/Networking/Router.hpp
+++ Program Files/Header Files/Networking/Router.hpp
@@ -1,15 +1,36 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 
-#include "Packet.hpp"
+#include <AccessControlList.hpp>
+#include <AlertSystem.hpp>
+#include <Logger.hpp>
+#include <Packet.hpp>
+#include <PacketForwarder.hpp>
+#include <PacketValidator.hpp>
+#include <RoutingEngine.hpp>
+#include <RoutingTable.hpp>
+#include <ThreatDetector.hpp>
+#include <ValidationError.hpp>
 
 class Router
 {
 private:
 
     std::string Name;
+
+    Logger RouterLogger;
+
+    RoutingTable RouterRoutingTable;
+    RoutingEngine RouterRoutingEngine;
+
+    PacketForwarder Forwarder;
+    PacketValidator Validator;
+
+    AccessControlList ACL;
+
+    ThreatDetector Detector;
+    AlertSystem Alerts;
 
 public:
 
@@ -18,13 +39,60 @@ public:
     {
     }
 
-    inline void ReceivePacket(const Packet& packet)
+    inline RoutingTable& GetRoutingTable()
     {
-        std::cout
-            << "[Router] Packet received at "
-            << Name
-            << " from "
-            << packet.SourceIP
-            << '\n';
+        return RouterRoutingTable;
+    }
+
+    inline AccessControlList& GetACL()
+    {
+        return ACL;
+    }
+
+    inline void ReceivePacket(Packet& packet)
+    {
+        RouterLogger.Info("Router " + Name + " received packet");
+
+        const auto validation = Validator.Validate(packet);
+
+        if (!validation.has_value())
+        {
+            RouterLogger.Error(validation.error().Message);
+            return;
+        }
+
+        if (!ACL.IsAllowed(packet))
+        {
+            RouterLogger.Warning( "Packet denied by ACL");
+
+            Detector.RegisterDeniedPacket();
+
+            if (Detector.GetSuspiciousAttempts() >= 3)
+            {
+                Alerts.RaiseAlert("Repeated denied packet activity detected");
+            }
+
+            return;
+        }
+
+        --packet.TTL;
+
+        if (packet.TTL <= 0)
+        {
+            RouterLogger.Warning("Packet TTL expired");
+            return;
+        }
+
+        auto resolvedRoute = RouterRoutingEngine.ResolveRoute(RouterRoutingTable, packet.DestinationIP);
+
+        if (!resolvedRoute.has_value())
+        {
+            RouterLogger.Warning("No route found for destination");
+            return;
+        }
+
+        Forwarder.ForwardPacket(packet, resolvedRoute->NextHop);
+
+        RouterLogger.Info("Packet forwarded successfully");
     }
 };
