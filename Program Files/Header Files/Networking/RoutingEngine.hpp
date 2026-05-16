fcde41cb06bedaa7190c0bf0ec1da6b80adce72a
Muhammad2775 <muhammad.moazzam2775@gmail.com>
2026-05-16 13:28:25 -0700

    Add ResolveRoute method to RoutingEngine for IP resolution

--- Program Files/Header Files/Networking/RoutingEngine.hpp
+++ Program Files/Header Files/Networking/RoutingEngine.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <string>
+#include <optional>
+
+#include <RoutingTable.hpp>
+
 class RoutingEngine
 {
+public:
+
+    inline std::optional<RouteEntry> ResolveRoute(const RoutingTable& routingTable, const std::string& destinationIP)
+    {
+        for (const RouteEntry& route : routingTable.GetRoutes())
+        {
+            if (destinationIP.starts_with(route.DestinationNetwork))
+            {
+                return route;
+            }
+        }
+
+        return std::nullopt;
+    }
 };
