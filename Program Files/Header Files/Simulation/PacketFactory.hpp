6a448237e381bbb7d5fe2646231be8ee5b35ef32
Muhammad2775 <muhammad.moazzam2775@gmail.com>
2026-05-16 13:55:32 -0700

    Implement packet creation functions in PacketFactory

--- Program Files/Header Files/Simulation/PacketFactory.hpp
+++ Program Files/Header Files/Simulation/PacketFactory.hpp
@@ -1,5 +1,74 @@
 #pragma once
 
-class PacketFactory
+#include <Packet.hpp>
+
+namespace PacketFactory
 {
-};
+    inline Packet
+    CreateTCPPacket(
+        const std::string& sourceIP,
+        const std::string& destinationIP,
+        int port)
+    {
+        return Packet
+        {
+            .SourceIP = sourceIP,
+            .DestinationIP = destinationIP,
+            .Protocol = ProtocolType::TCP,
+            .TTL = 64,
+            .Port = port,
+            .PayloadSize = 1024,
+            .IsMalformed = false
+        };
+    }
+
+    inline Packet
+    CreateUDPPacket(
+        const std::string& sourceIP,
+        const std::string& destinationIP,
+        int port)
+    {
+        return Packet
+        {
+            .SourceIP = sourceIP,
+            .DestinationIP = destinationIP,
+            .Protocol = ProtocolType::UDP,
+            .TTL = 64,
+            .Port = port,
+            .PayloadSize = 512,
+            .IsMalformed = false
+        };
+    }
+
+    inline Packet
+    CreateICMPPacket(
+        const std::string& sourceIP,
+        const std::string& destinationIP)
+    {
+        return Packet
+        {
+            .SourceIP = sourceIP,
+            .DestinationIP = destinationIP,
+            .Protocol = ProtocolType::ICMP,
+            .TTL = 128,
+            .Port = 0,
+            .PayloadSize = 64,
+            .IsMalformed = false
+        };
+    }
+
+    inline Packet
+    CreateMalformedPacket()
+    {
+        return Packet
+        {
+            .SourceIP = "999.999.999.999",
+            .DestinationIP = "192.168.1.10",
+            .Protocol = ProtocolType::UNKNOWN,
+            .TTL = -1,
+            .Port = -1,
+            .PayloadSize = 0,
+            .IsMalformed = true
+        };
+    }
+}
