6ad2ff3e61183cb8884a03b9e4524d6e1930fda1
Muhammad2775 <muhammad.moazzam2775@gmail.com>
2026-05-16 13:34:06 -0700

    Implement Access Control List with rule management

--- Program Files/Header Files/Security/AccessControlList.hpp
+++ Program Files/Header Files/Security/AccessControlList.hpp
@@ -1,5 +1,38 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include <Packet.hpp>
+
+struct ACLRule
+{
+    std::string BlockedSourceIP;
+};
+
 class AccessControlList
 {
+private:
+
+    std::vector<ACLRule> Rules;
+
+public:
+
+    inline void AddRule(const ACLRule& rule)
+    {
+        Rules.emplace_back(rule);
+    }
+
+    inline bool IsAllowed(const Packet& packet) const
+    {
+        for (const ACLRule& rule : Rules)
+        {
+            if (packet.SourceIP == rule.BlockedSourceIP)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 };
