c105b93616cab3109c9d0fe8bd8cce647510196d
Muhammad2775 <muhammad.moazzam2775@gmail.com>
2026-05-16 14:04:47 -0700

    Implement traffic classification functions in Utilities

--- Program Files/Header Files/Utilities/Utilities.hpp
+++ Program Files/Header Files/Utilities/Utilities.hpp
@@ -1,5 +1,27 @@
 #pragma once
 
-class Utilities
+#include <IPAddressUtilities.hpp>
+#include <NetworkUtilities.hpp>
+
+namespace Utilities
 {
-};
+    inline bool
+    IsInternalTraffic(
+        const std::string& sourceIP,
+        const std::string& destinationIP)
+    {
+        return
+            IPAddressUtilities::IsPrivateIPAddress(sourceIP)
+            &&
+            IPAddressUtilities::IsPrivateIPAddress(destinationIP);
+    }
+
+    inline bool
+    IsExternalTraffic(
+        const std::string& sourceIP,
+        const std::string& destinationIP)
+    {
+        return
+            !IsInternalTraffic(sourceIP, destinationIP);
+    }
+}
